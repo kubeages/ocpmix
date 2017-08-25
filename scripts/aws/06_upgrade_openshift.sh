789d4d2705e7464d5e8d137ff74c56d6d1916f2f
Antonio Gallego <antoniogallego@gmail.com>
2017-08-25 09:10:35 +0200

    Update 06_upgrade_openshift.sh

--- scripts/aws/06_upgrade_openshift.sh
+++ scripts/aws/06_upgrade_openshift.sh
@@ -1,1 +1,13 @@
-ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/upgrades/v3_4/upgrade.yml
+#!/bin/bash
+# bastion tasks
+subscription-manager repos --disable="rhel-7-server-ose-3.5-rpms" --enable="rhel-7-server-ose-3.6-rpms"
+yum clean all
+yum update -y atomic-openshift-utils
+
+# nodes tasks
+ansible all -a "subscription-manager repos --disable=\"rhel-7-server-ose-3.5-rpms\" --enable=\"rhel-7-server-ose-3.6-rpms\""
+ansible all -a "yum clean all"
+ansible all -a "yum update -y atomic-openshift-utils"
+
+# run update playbook
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/upgrades/v3_6/upgrade.yml
