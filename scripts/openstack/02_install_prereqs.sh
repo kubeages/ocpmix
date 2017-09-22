21b40068ad206885a22f274a8b08a60f5aeba153
Antonio Gallego <antoniogallego@gmail.com>
2017-09-22 09:59:08 +0200

    Updated to 3.6 requirements

--- scripts/openstack/02_install_prereqs.sh
+++ scripts/openstack/02_install_prereqs.sh
@@ -5,7 +5,7 @@ ALL_HOSTS="master1 infranode1 appnode1 appnode2 appnode3"
 for host in $ALL_HOSTS
 do
 
-ssh -tt $host "sudo yum install wget git net-tools bind-utils iptables-services bridge-utils bash-completion -y; sudo yum update -y; sudo yum install atomic-openshift-utils -y; sudo yum install atomic-openshift-excluder atomic-openshift-docker-excluder -y; atomic-openshift-excluder unexclude; atomic-openshift-docker-excluder unexclude; sudo yum install docker -y"
+ssh -tt $host "sudo yum install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct -y; sudo yum update -y; sudo yum install atomic-openshift-utils -y; sudo yum install docker-1.12.6 -y"
 
 [ $? -ne 0 ] && echo "** FAIL **"
 
