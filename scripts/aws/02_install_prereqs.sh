1417bc88f4ef4657c9abf61a030698233f1c2b01
Antonio Gallego <antoniogallego@gmail.com>
2017-10-09 00:07:29 +0200

    Update 02_install_prereqs.sh

--- scripts/aws/02_install_prereqs.sh
+++ scripts/aws/02_install_prereqs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ALL_HOSTS="localhost ip-172-31-7-149.ec2.internal ip-172-31-5-169.ec2.internal ip-172-31-13-104.ec2.internal ip-172-31-6-112.ec2.internal"
+ALL_HOSTS="localhost ip-172-31-4-76.ec2.internal ip-172-31-11-202.ec2.internal ip-172-31-8-217.ec2.internal ip-172-31-0-92.ec2.internal"
 
 for host in $ALL_HOSTS
 do
 
-ssh -tt $host "yum install wget git net-tools bind-utils iptables-services bridge-utils bash-completion -y; yum update -y; yum install atomic-openshift-utils -y; yum install atomic-openshift-excluder atomic-openshift-docker-excluder -y; atomic-openshift-excluder unexclude; atomic-openshift-docker-excluder unexclude; yum install docker -y"
+ssh -tt $host "yum install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct -y; yum update -y; yum install atomic-openshift-utils -y; yum install docker -y"
 
 [ $? -ne 0 ] && echo "** FAIL **"
 
