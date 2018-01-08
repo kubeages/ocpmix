2f99aad1d91ea25b102b29b0634327e56238d445
Antonio Gallego <antoniogallego@gmail.com>
2018-01-08 15:51:43 +0100

    Update 01_subscribe_nodes.sh

--- scripts/gcp/01_subscribe_nodes.sh
+++ scripts/gcp/01_subscribe_nodes.sh
@@ -6,8 +6,8 @@ for host in $ALL_HOSTS
 do
 
 echo "Registering node $host"
-ssh -tt $host "sudo subscription-manager register --username=agallego2 --password=redhat123"
-ssh -tt $host "sudo subscription-manager attach --pool=8a85f98c5f9a9be6015f9ac7fd8b0392"
+ssh -tt $host "sudo subscription-manager register --username=RHN_USER --password=RHN_PWD"
+ssh -tt $host "sudo subscription-manager attach --pool=POOL_ID"
 echo "Enabling repos for $host"
 ssh -tt $host "sudo subscription-manager repos --disable=* --enable='rhel-7-server-rpms' --enable='rhel-7-server-extras-rpms' --enable='rhel-7-server-ose-3.7-rpms' --enable='rhel-7-fast-datapath-rpms'"
 
