232a15d6f80452826d1a7bb84825c86027ee073a
Antonio Gallego <antoniogallego@gmail.com>
2018-01-22 11:24:27 +0100

    Fix in target docker-storage-setup

--- scripts/gcp/03_configure_docker_storage.sh
+++ scripts/gcp/03_configure_docker_storage.sh
@@ -1,1 +1,1 @@
-ansible all -m copy -a "src=03_docker_storage_setup dest=/etc/sysconfig/docker_storage_setup"
+ansible all -m copy -a "src=03_docker_storage_setup dest=/etc/sysconfig/docker-storage-setup"
