1de4d0c586db7adf840d2543e4ad447646cb6d18
ages <antoniogallego@gmail.com>
2018-04-24 14:26:39 +0200

    added bind-utils and net-tools to bastion list of sw

--- scripts/gcp/00_prepare_bastion.sh
+++ scripts/gcp/00_prepare_bastion.sh
@@ -4,4 +4,4 @@ sudo subscription-manager refresh
 sudo subscription-manager attach --pool=POOL_ID
 sudo subscription-manager repos --disable=* --enable='rhel-7-server-rpms' --enable='rhel-7-server-extras-rpms' --enable='rhel-7-server-ose-3.9-rpms' --enable='rhel-7-fast-datapath-rpms' --enable='rhel-7-server-ansible-2.4-rpms'
 sudo yum -y update
-sudo yum install -y atomic-openshift-utils atomic-openshift-clients wget git screen bash-completion tmux
+sudo yum install -y atomic-openshift-utils atomic-openshift-clients wget git screen bash-completion tmux bind-utils net-tools
