b20ec39ccd5edfe00de28af7e2d9b1b65aaf16b2
ages <antoniogallego@gmail.com>
2018-04-24 14:11:52 +0200

    update prepare bastion script

--- scripts/gcp/00_prepare_bastion.sh
+++ scripts/gcp/00_prepare_bastion.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+sudo subscription-manager register --username=RHN_USER --password=RHN_PWD
+sudo subscription-manager refresh
+sudo subscription-manager attach --pool=POOL_ID
+sudo subscription-manager repos --disable=* --enable='rhel-7-server-rpms' --enable='rhel-7-server-extras-rpms' --enable='rhel-7-server-ose-3.9-rpms' --enable='rhel-7-fast-datapath-rpms' --enable='rhel-7-server-ansible-2.4-rpms'
 sudo yum -y update
 sudo yum install -y atomic-openshift-utils atomic-openshift-clients wget git screen bash-completion 
